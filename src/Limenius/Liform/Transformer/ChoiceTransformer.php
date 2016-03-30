90520a86f54efe339c80c9e4c7ece274371d4e24
nacho <nacho@limenius.com>
2016-03-31 00:02:57 +0200

    Enum titles

--- src/Limenius/Liform/Transformer/ChoiceTransformer.php
+++ src/Limenius/Liform/Transformer/ChoiceTransformer.php
@@ -11,17 +11,21 @@ class ChoiceTransformer
         $formView = $form->createView();
 
         $choices = [];
+        $titles = [];
         foreach ($formView->vars['choices'] as $choiceView) {
             if ($choiceView instanceof ChoiceGroupView) {
                 foreach ($choiceView->choices as $choiceItem) {
                     $choices[] = $choiceItem->value;
+                    $titles[] = $choiceItem->label;
                 }
             } else {
                 $choices[] = $choiceView->value;
+                $titles[] = $choiceView->label;
             }
         }
         return [
             'enum' => $choices,
+            'options' => ['enum_titles' => $titles],
             'type' => 'string'
             ];
 
