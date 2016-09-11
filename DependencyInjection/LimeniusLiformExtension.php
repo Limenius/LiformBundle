2c3d4ea038875213381d2f7f61a317b4ea5b49af
nacho <nacho@limenius.com>
2016-09-11 21:39:22 +0200

    Fix DI

--- DependencyInjection/LimeniusLiformExtension.php
+++ DependencyInjection/LimeniusLiformExtension.php
@@ -20,6 +20,7 @@ class LimeniusLiformExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('transformers.xml');
         $loader->load('services.xml');
 
     }
