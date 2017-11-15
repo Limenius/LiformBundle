16daecd8c038b48ec0bebd0f84d8e45c6be94037
Sjoerd Peters <sjoerd.peters@gmail.com>
2017-11-15 14:09:50 +0100

    Added tags to add transformers and extensions, added parameters for service classes

--- LimeniusLiformBundle.php
+++ LimeniusLiformBundle.php
@@ -2,8 +2,19 @@
 
 namespace Limenius\LiformBundle;
 
+use Limenius\LiformBundle\DependencyInjection\Compiler\ExtensionCompilerPass;
+use Limenius\LiformBundle\DependencyInjection\Compiler\TransformerCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LimeniusLiformBundle extends Bundle
 {
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new TransformerCompilerPass());
+        $container->addCompilerPass(new ExtensionCompilerPass());
+    }
 }
